83084195ccdb6ac16a657cfbc4186ebed7409a0b
hoang <hoanghuy4d@gmail.com>
2025-03-12 15:23:50 +0700

    add caculus.cpp

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -7,46 +7,40 @@ using std::cout;
 using std::endl;
 using std::stod;
 
-
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: cosine of x
-***/
 double myCos(double x) 
 {
-    return 0.0;
+    double result = 0.0;
+    double term = 1.0;
+    for (int i = 0; i < 10; i++) {
+        result += term;
+        term *= -x * x / ((2 * i + 2) * (2 * i + 1));
+    }
+    return result;
 }
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: sine of x
-***/
 double mySin(double x)
 {
-    return 0.0;
+    double result = 0.0;
+    double term = x;
+    for (int i = 0; i < 10; i++) {
+        result += term;
+        term *= -x * x / ((2 * i + 3) * (2 * i + 2));
+    }
+    return result;
 }
 
-
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: square root of x
-***/
 double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
-    return 0;
+    double result = x;
+    for (int i = 0; i < 10; i++) {
+        result = 0.5 * (result + x / result);
+    }
+    return result;
 }
